bb3a8b8af49e4d78e9840d895a5c88611573a71e
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-15 16:56:10 +0100

    ADD names.js OK

--- names.js
+++ names.js
@@ -1,2 +1,7 @@
 //Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 
+function nameGenerator (firstName, lastName){
+    return {firstName, lastName}
+}
+
+module.exports = nameGenerator;
