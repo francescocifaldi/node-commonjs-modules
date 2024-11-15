384cad34f858321a692c5fa52d1ea79d2f6390ef
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-15 17:08:57 +0100

    ADD people.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 console.log('hello model!')
 
-const hobbiesGenerator = require('./hobbies');
 const nameGenerator = require('./names')
+const hobbiesGenerator = require('./hobbies');
+const personGenerator = require('./people')
+
 
 console.log(nameGenerator('Umberto','Tozzi'));
 console.log(hobbiesGenerator('Nuoto','Fai da te','fotografia'))
+console.log(personGenerator())
--- people.js
+++ people.js
@@ -1,3 +1,15 @@
 // Importa la tua funzione da names.js
 // Importa la tua funzione da hobbies.js
 // Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies. All’interno della tua funzione, usa le tue due funzioni precedenti per costruire l’oggetto.
+
+const nameGenerator = require('./names')
+const hobbiesGenerator = require('./hobbies');
+
+
+function personGenerator(){
+    const {firstName, lastName} = nameGenerator('Mickey','Mouse')
+    const {hobbies} = hobbiesGenerator('A','B','C')
+    return {firstName, lastName, hobbies}
+}
+
+module.exports=personGenerator;
