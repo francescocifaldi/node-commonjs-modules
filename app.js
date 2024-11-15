5a647820dc2ff5e2d054812379ef7f20a85f0c69
francescocifaldi <francesco.cifaldi@gmail.com>
2024-11-15 16:59:35 +0100

    ADD hobbies.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log('hello model!')
 
+const hobbiesGenerator = require('./hobbies');
 const nameGenerator = require('./names')
 
 console.log(nameGenerator('Umberto','Tozzi'));
+console.log(hobbiesGenerator('Nuoto','Fai da te','fotografia'))
